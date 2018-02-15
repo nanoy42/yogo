e6ca485821bdb77d4943755df34ec116dde5eea9
Hugo LEVY-FALK <klafyvel@gmail.com>
2018-02-15 23:35:01 +0100

    Settings de debug

--- yogo/settings.py
+++ yogo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
--- yogo/wsgi.py
+++ yogo/wsgi.py
@@ -9,13 +9,15 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 import sys
+from .settings import DEBUG
 
-VIRTUALENV_LOC = '/var/www/yogo/env_yogo'
+if not DEBUG:
+    VIRTUALENV_LOC = '/var/www/yogo/env_yogo'
 
-activate_env=os.path.join(VIRTUALENV_LOC, 'bin/activate_this.py')
-exec(compile(open(activate_env, "rb").read(), activate_env, 'exec'), {'__file__':activate_env})
-sys.path.append('/var/www/yogo')
-sys.path.append('/var/www/yogo/yogo')
+    activate_env=os.path.join(VIRTUALENV_LOC, 'bin/activate_this.py')
+    exec(compile(open(activate_env, "rb").read(), activate_env, 'exec'), {'__file__':activate_env})
+    sys.path.append('/var/www/yogo')
+    sys.path.append('/var/www/yogo/yogo')
 
 
 from django.core.wsgi import get_wsgi_application
