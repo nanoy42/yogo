88cffa465a6850f9866121b462e37011017c22a5
Hugo LEVY-FALK <klafyvel@gmail.com>
2018-02-16 08:16:33 +0100

    PEP8 dans views.

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .models import Task, Project, Tag
-from .forms import ProjectForm, addMemberForm, TagForm, TaskForm
+from .forms import ProjectForm, AddMemberForm, TagForm, TaskForm
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -9,14 +9,17 @@ from yogo.acl import can_edit_project, admin_required, member_required
 
 
 @login_required
-def myprojects(request):
+def my_projects(request):
     projects = request.user.membered_projects.all()
     active = 2
-    return render(request, 'projects/myprojects.html', {'projects': projects, 'active': active})
+    return render(request, 'projects/myprojects.html', {
+        'projects': projects,
+        'active': active
+    })
 
 
 @login_required
-def mytasks(request):
+def my_tasks(request):
     active = 3
     todo_tasks = Task.objects.filter(
         userAssigned=request.user).filter(status=Task.State.TODO)
@@ -24,11 +27,16 @@ def mytasks(request):
         userAssigned=request.user).filter(status=Task.State.DOING)
     done_tasks = Task.objects.filter(
         userAssigned=request.user).filter(status=Task.State.DONE)
-    return render(request, 'projects/mytasks.html', {'active': active, 'todo': todo_tasks, 'doing': doing_tasks, 'done': done_tasks})
+    return render(request, 'projects/mytasks.html', {
+        'active': active,
+        'todo': todo_tasks,
+        'doing': doing_tasks,
+        'done': done_tasks
+    })
 
 
 @login_required
-def newProject(request):
+def new_project(request):
     active = 2
     form = ProjectForm(request.POST or None)
     if(form.is_valid()):
@@ -37,8 +45,17 @@ def newProject(request):
         form.instance.users.add(request.user)
         form.save()
         messages.success(request, "Projet créé.")
-        return redirect(reverse('projects:project', kwargs={'pk':form.instance.pk}))
-    return render(request, 'form.html', {'form': form, 'active': active, 'title': "Nouveau projet", 'bouton': 'Créer le projet', 'icon': 'star'})
+        return redirect(reverse(
+            'projects:project',
+            kwargs={'pk': form.instance.pk}
+        ))
+    return render(request, 'form.html', {
+        'form': form,
+        'active': active,
+        'title': "Nouveau projet",
+        'bouton': 'Créer le projet',
+        'icon': 'star'
+    })
 
 
 @login_required
@@ -51,9 +68,18 @@ def project(request, pk, project_form=None, member_form=None):
     taken_tasks = []
     for user in project.users.all():
         taken_tasks.append(project.task_set.filter(userAssigned=user).count())
-    memberForm = addMemberForm(request.POST or None, projectId=pk)
+    memberForm = AddMemberForm(request.POST or None, projectId=pk)
     projectForm = ProjectForm(request.POST or None, instance=project)
-    return render(request, 'projects/project.html', {'active': 2, 'project': project, 'todo': task_todo, 'doing': task_doing, 'done': task_done, 'taken_tasks': taken_tasks, 'addMemberForm': memberForm, 'projectForm': projectForm})
+    return render(request, 'projects/project.html', {
+        'active': 2,
+        'project': project,
+        'todo': task_todo,
+        'doing': task_doing,
+        'done': task_done,
+        'taken_tasks': taken_tasks,
+        'addMemberForm': memberForm,
+        'projectForm': projectForm
+    })
 
 
 @login_required
@@ -64,7 +90,7 @@ def update_project_info(request, pk):
     if project_form.is_valid():
         project_form.save()
         messages.success(request, 'Le projet a bien été modifié')
-        return redirect(reverse('projects:project', kwargs={'pk':pk}))
+        return redirect(reverse('projects:project', kwargs={'pk': pk}))
     messages.error(request, 'Erreur dans la modification du projet.')
     return project(request, pk, project_form=project_form)
 
@@ -73,7 +99,7 @@ def update_project_info(request, pk):
 @can_edit_project
 def add_user_to_project(request, pk):
     project = get_object_or_404(Project, pk=pk)
-    member_form = addMemberForm(request.POST or None, projectId=pk)
+    member_form = AddMemberForm(request.POST or None, projectId=pk)
     if member_form.is_valid():
         member = member_form.cleaned_data['member']
         if member in project.users.all():
@@ -81,13 +107,13 @@ def add_user_to_project(request, pk):
         else:
             project.users.add(member)
             messages.success(request, "L'utilisateur a bien été ajouté")
-        return redirect(reverse('projects:project', kwargs={'pk':pk}))
+        return redirect(reverse('projects:project', kwargs={'pk': pk}))
     return project(request, pk, member_form=member_form)
 
 
 @login_required
 @can_edit_project
-def deleteUserFromProject(request, pk, user_id):
+def delete_user_from_project(request, pk, user_id):
     project = Project.objects.get(pk=pk)
     user = User.objects.get(pk=user_id)
     if(user not in project.users.all()):
@@ -100,14 +126,18 @@ def deleteUserFromProject(request, pk, user_id):
 
 @login_required
 @admin_required
-def manageProjects(request):
+def manage_projects(request):
     projects = Project.objects.all()
-    return render(request, 'projects/manageProjects.html', {'projects': projects})
+    return render(
+        request,
+        'projects/manageProjects.html',
+        {'projects': projects}
+    )
 
 
 @login_required
 @can_edit_project
-def changeState(request, pk):
+def change_state(request, pk):
     try:
         project = Project.objects.get(pk=pk)
     except:
@@ -118,15 +148,15 @@ def changeState(request, pk):
         messages.success(request, 'Le statut du projet a bien été modifié')
     else:
         messages.error(request, 'Ce projet n\'existe pas')
-    return redirect(request.META.get('HTTP_REFERER', '/'))
+    return redirect(redirect('home'))
 
 
 @login_required
 @can_edit_project
-def deleteProject(request, pk):
+def delete_project(request, pk):
     try:
         project = Project.objects.get(pk=pk)
-    except:
+    except DoesNotExist:
         messages.error(request, 'Ce projet n\'existe pas')
         return redirect(reverse('home'))
     project.delete()
@@ -136,91 +166,109 @@ def deleteProject(request, pk):
 
 @login_required
 @can_edit_project
-def newTag(request, pk):
+def new_tag(request, pk):
     form = TagForm(request.POST or None)
     project = get_object_or_404(Project, pk=pk)
     if(form.is_valid()):
         form.instance.project = project
         form.save()
         messages.success(request, 'Le tag a bien été créé')
-        return redirect(reverse('projects:project', kwargs={"pk":pk}))
-    return render(request, 'form.html', {'form': form, 'title': 'Nouveau tag', 'bouton': 'Créer le tag', 'icon': 'star'})
+        return redirect(reverse('projects:project', kwargs={"pk": pk}))
+    return render(request, 'form.html', {
+        'form': form,
+        'title': 'Nouveau tag',
+        'bouton': 'Créer le tag',
+        'icon': 'star'
+    })
 
 
 @login_required
 @can_edit_project
-def editTag(request, pk, tagId):
+def edit_tag(request, pk, tagId):
     try:
         tag = Tag.objects.get(pk=tagId)
-    except:
+    except DoesNotExist:
         messages.error(request, 'Ce tag n\'existe pas')
         return redirect(reverse('home'))
     form = TagForm(request.POST or None, instance=tag)
     if(form.is_valid()):
         form.save()
         messages.success(request, 'Le tag a bien été modifié')
-        return redirect(reverse('projects:project', kwargs={'pk':pk}))
-    return render(request, 'form.html', {'form': form, 'title': 'Modification du tag ' + tag.name, 'bouton': 'Modifier', 'icon': 'pencil-alt'})
+        return redirect(reverse('projects:project', kwargs={'pk': pk}))
+    return render(request, 'form.html', {
+        'form': form,
+        'title': 'Modification du tag ' + tag.name,
+        'bouton': 'Modifier',
+        'icon': 'pencil-alt'
+    })
 
 
 @login_required
 @can_edit_project
-def deleteTag(request, pk, tagId):
+def delete_tag(request, pk, tagId):
     try:
         tag = Tag.objects.get(pk=tagId)
-    except:
+    except DoesNotExist:
         messages.error(request, "Ce tag n'existe pas")
         return redirect(reverse('home'))
     tag.delete()
     messages.success(request, "Le tag a bien été supprimé")
-    return redirect(reverse('projects:project', kwargs={'pk':pk}))
+    return redirect(reverse('projects:project', kwargs={'pk': pk}))
 
 
 @login_required
 @member_required
-def newTask(request, pk):
+def new_task(request, pk):
     active = 2
     project = Project.objects.get(pk=pk)
     form = TaskForm(project, request.POST or None)
     if(form.is_valid()):
         form.save()
         messages.success(request, 'La tâche a été créée.')
-        return redirect(reverse('projects:project', kwargs={"pk":pk}))
-    return render(request, 'form.html', {'active': active, 'form': form, 'icon': 'star', 'bouton': 'Créer la tâche', 'title': 'Nouvelle tâche de '+project.title})
+        return redirect(reverse('projects:project', kwargs={"pk": pk}))
+    return render(request, 'form.html', {
+        'active': active,
+        'form': form,
+        'icon': 'star',
+        'bouton': 'Créer la tâche',
+        'title': 'Nouvelle tâche de '+project.title
+    })
 
 
 @login_required
 @can_edit_project
-def changeTaskStatus(request, pk, taskId, newStatus):
-    cor = {'todo': Task.State.TODO, 'doing': Task.State.DOING, 'done': Task.State.DONE}
+def change_task_status(request, pk, taskId, newStatus):
+    cor = {'todo': Task.State.TODO,
+           'doing': Task.State.DOING, 'done': Task.State.DONE}
     try:
         task = Task.objects.get(pk=taskId)
-    except:
+    except DoesNotExist:
         messages.error(request, "La tâche n'existe pas")
         return redirect(reverse('home'))
     try:
         task.status = cor[newStatus]
-    except:
+    except IndexError:
         messages.error(request, "Le status demandé n'existe pas")
         return redirect(reverse('home'))
     task.save()
     messages.success(request, "La tâche est passée en " + newStatus)
-    url_next = request.GET.get('next', reverse('projects:project', kwargs={'pk': pk}))
+    url_next = request.GET.get('next', reverse(
+        'projects:project', kwargs={'pk': pk}))
     return redirect(url_next)
 
 
 @login_required
-def deleteTask(request, taskId):
+def delete_task(request, taskId):
     try:
         task = Task.objects.get(pk=taskId)
-    except:
+    except DoesNotExist:
         messages.error(request, "La tâche n'existe pas")
         return redirect(reverse('home'))
     task.delete()
     messages.success(request, "La tâche a bien été supprimée")
     next_url = request.GET.get(
         'next',
-        reverse('projects:project', kwargs={'pk':task.project.pk})
+        reverse('projects:project', kwargs={'pk': task.project.pk})
     )
     return redirect(next_url)
 
@@ -229,31 +277,40 @@ def deleteTask(request, taskId):
 def paps(request, taskId):
     try:
         task = Task.objects.get(pk=taskId)
-    except:
+    except DoesNotExist:
         messages.error(request, "La tâche n'existe pas")
         return redirect(reverse('home'))
     task.userAssigned = request.user
     task.save()
     messages.success(request, "Vous avez paspé la tâche. Au boulot !")
-    return redirect(reverse('projects:project', kwargs={'pk': task.project.pk}))
+    return redirect(reverse(
+        'projects:project',
+        kwargs={'pk': task.project.pk}
+    ))
 
 
 @login_required
 def depaps(request, taskId):
     try:
         task = Task.objects.get(pk=taskId)
-    except:
+    except DoesNotExist:
         messages.error(request, "La tâche n'existe pas")
-        return redirect(reverse('shome'))
+        return redirect(reverse('home'))
     if(task.userAssigned != request.user):
-        messages.error(request, "Vous ne pouvez pas depaps une tâche que vous n'avez pas papsée")
-        return redirect(reverse('projects:project', kwargs={'pk': task.project.pk}))
+        messages.error(
+            request,
+            "Vous ne pouvez pas depaps une tâche que vous n'avez pas papsée"
+        )
+        return redirect(reverse(
+            'projects:project',
+            kwargs={'pk': task.project.pk}
+        ))
     else:
         task.userAssigned = None
         task.save()
         messages.success(request, "Depaps réussi (flemmard)")
         next_url = request.GET.get(
             'next',
-            reverse('projects:project', kwargs={'pk':task.project.pk})
+            reverse('projects:project', kwargs={'pk': task.project.pk})
         )
         return redirect(next_url)
