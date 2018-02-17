3c776ad780b590310b747507d7f409561a6fcc0c
Hugo LEVY-FALK <klafyvel@gmail.com>
2018-02-18 00:25:15 +0100

    login_required dans les vues générales + PEP8 mon amour.

--- yogo/views.py
+++ yogo/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import logout, login, authenticate
 from .forms import LoginForm
 from django.contrib import messages
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.decorators import login_required
 from projects.models import Project
 
 from yogo.acl import admin_required
@@ -17,35 +18,56 @@ def home(request):
 def login_view(request):
     form = LoginForm(request.POST or None)
     if(form.is_valid()):
-        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+        user = authenticate(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password'])
         if(user is not None):
             login(request, user)
             messages.success(request, "Bienvenue " + user.username)
             return redirect(reverse('home'))
         else:
-            messages.error(request, "Nom d'utilisateur et/ou mot de passe incorrect")
-    return render(request, 'form.html', {'title': 'Connexion', 'form': form, 'bouton':' Se connecter', 'icon': 'sign-in-alt', 'active': 4})
+            messages.error(
+                request, "Nom d'utilisateur et/ou mot de passe incorrect")
+    return render(request, 'form.html', {
+        'title': 'Connexion',
+        'form': form,
+        'bouton': ' Se connecter',
+        'icon': 'sign-in-alt',
+        'active': 4
+    })
 
 
+@login_required
 def logout_view(request):
     logout(request)
     return redirect(reverse('home'))
 
+
+@login_required
 @admin_required
 def manageUsers(request):
     users = User.objects.all()
     admin, _ = Group.objects.get_or_create(name="admin")
-    return render(request, 'yogo/manageUsers.html',{'users': users, 'admin':admin})
+    return render(request, 'yogo/manageUsers.html', {
+        'users': users,
+        'admin': admin
+    })
 
+
+@login_required
 def profile(request):
     createdProjects = Project.objects.filter(owner=request.user).count()
-    return render(request, 'yogo/profile.html', {'createdProjects':createdProjects})
+    return render(request, 'yogo/profile.html', {
+        'createdProjects': createdProjects
+    })
 
+
+@login_required
 @admin_required
 def add_admin(request, user_id):
     try:
         user = User.objects.get(pk=user_id)
-    except:
+    except DoesNotExist:
         messages.error(request, "L'utilisateur n'existe pas")
         return redirect(reverse('home'))
     admin, _ = Group.objects.get_or_create(name="admin")
@@ -56,11 +78,13 @@ def add_admin(request, user_id):
     messages.success(request, user.username + " a été passé administrateur")
     return redirect(reverse('manageUsers'))
 
+
+@login_required
 @admin_required
 def remove_admin(request, user_id):
     try:
         user = User.objects.get(pk=user_id)
-    except:
+    except DoesNotExist:
         messages.error(request, "L'utilisateur n'existe pas")
         return redirect(reverse('home'))
     admin, _ = Group.objects.get_or_create(name="admin")
@@ -72,11 +96,12 @@ def remove_admin(request, user_id):
     return redirect(reverse('manageUsers'))
 
 
+@login_required
 @admin_required
 def remove_user(request, user_id):
     try:
         user = User.objects.get(pk=user_id)
-    except:
+    except DoesNotExist:
         messages.error(request, "L'utilisateur n'existe pas")
         return redirect(reverse('home'))
     user.delete()
